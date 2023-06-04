257d9d22adc34511aad19b1211136c801a45e6aa
Nick Partalis <nickpartalis@hotmail.com>
2023-06-04 19:06:39 +0300

    Add solved exercises 2.6 - 2.10 (phonebook)

---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,12 @@
+const Filter = ({ filter, handleChange }) => {
+  return (
+    <p>
+      Filter shown with <input 
+        value={filter} 
+        onChange={handleChange} 
+      />
+    </p>
+  )
+}
+
+export default Filter
