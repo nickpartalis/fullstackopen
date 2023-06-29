0fda2e57598586eb73c7081c2bc9ff09a89a15d8
Nick Partalis <nickpartalis@hotmail.com>
2023-06-29 20:57:05 +0300

    Add solved exercises 3.19 - 3.22

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,40 +1,41 @@
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
-const Person = require("./models/person")
+const Person = require('./models/person')
 
 // MIDDLEWARE
 app.use(express.json())
 app.use(cors())
-app.use(express.static("build"))
+app.use(express.static('build'))
 
-morgan.token("postBody", (req, res) => {
-  if (req.method === "POST") {
-    const { name, number, id } = req.body
+morgan.token('postBody', (req, res) => {
+  if (req.method === 'POST') {
+    const { name, number } = req.body
     return JSON.stringify({ name, number })
   }
 })
-app.use(morgan((tokens, req, res) => {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, "content-length"), "-",
-    tokens["response-time"](req, res), "ms",
-    tokens.postBody(req, res)
-  ].join(" ")
-}))
+app.use(morgan((tokens, req, res) => [
+  tokens.method(req, res),
+  tokens.url(req, res),
+  tokens.status(req, res),
+  tokens.res(req, res, 'content-length'), '-',
+  tokens['response-time'](req, res), 'ms',
+  tokens.postBody(req, res),
+].join(' ')))
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.message)
 
   if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
+    return res.status(400).json({ error: 'malformatted id' })
   }
-  next(err)
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
+  }
+  return next(err)
 }
 
 const unknownEndpoint = (req, res) => {
@@ -42,11 +43,11 @@ const unknownEndpoint = (req, res) => {
 }
 
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("Phonebook API")
+app.get('/', (req, res) => {
+  res.send('Phonebook API')
 })
 
-app.get("/info", async (req, res) => {
+app.get('/info', async (req, res) => {
   const count = await Person.count()
   const time = new Date()
   const message = `Phonebook has info for ${count} people`
@@ -56,58 +57,65 @@ app.get("/info", async (req, res) => {
   `)
 })
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then(notes => {
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((notes) => {
     res.json(notes)
   })
 })
 
-app.post("/api/persons", (req, res) => {
-  const body = req.body
-  if (!body.name || !body.number) 
-    return res.status(400).json({ error: "missing name or number" })
+app.post('/api/persons', (req, res, next) => {
+  const { body } = req
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'Missing name or number' })
+  }
 
   const person = new Person({
     name: body.name,
-    number: body.number
+    number: body.number,
   })
-  
-  person.save().then(savedPerson => {
-    res.status(201).json(person)
-  })
+
+  person.save()
+    .then((savedPerson) => {
+      res.status(201).json(person)
+    })
+    .catch((err) => next(err))
 })
 
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
-    .then(person => {
+    .then((person) => {
       if (person) {
         res.json(person)
       } else {
-        res.status(404).end() 
+        res.status(404).end()
       }
     })
-    .catch(err => next(err))
+    .catch((err) => next(err))
 })
 
-app.delete("/api/persons/:id", (req, res, next) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(result => {
+    .then((result) => {
       res.status(204).end()
     })
-    .catch(err => next(err))
+    .catch((err) => next(err))
 })
 
-app.put("/api/persons/:id", (req, res, next) => {
+app.put('/api/persons/:id', (req, res, next) => {
   const person = {
     name: req.body.name,
-    number: req.body.number
+    number: req.body.number,
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
-    .then(updatedPerson => {
+  Person.findByIdAndUpdate(
+    req.params.id,
+    person,
+    { new: true, runValidators: true, context: 'query' },
+  )
+    .then((updatedPerson) => {
       res.json(updatedPerson)
     })
-    .catch(err => next(err))
+    .catch((err) => next(err))
 })
 
 app.use(unknownEndpoint)
