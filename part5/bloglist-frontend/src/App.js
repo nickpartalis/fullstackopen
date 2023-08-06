93dae03f6157ff1acdbf26d7b7d7dc6631f845fd
Nick Partalis <nickpartalis@hotmail.com>
2023-08-06 16:41:40 +0300

    Fix infinite GET request bug

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -14,8 +14,10 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll()
-      .then(blogs => setBlogs(blogs))  
-  }, [blogs])
+      .then(blogs => setBlogs(blogs))
+    console.log(blogs)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(blogs)]) // prevents infinite GET request loop
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
